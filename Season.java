edce0fd5e6b2ca96403804d740a5f76d7bb1f71c
Nishant Inamdar <nishant.inamdar@DM1379.datametica.com>
2021-11-24 15:59:50 +0530

    Updated Season.java

--- Season.java
+++ Season.java
@@ -5,11 +5,11 @@ public enum Season {
     FALL(12),SUMMER(14),RAINY, WINTER;       // these 4 enum objects will be created internally.  Must be First Line.
     int number;
     Season(){                       // private by default
-        System.out.println("Default Constructor Called");
+        System.out.println("Default Constructor Initiliazed");
     }
     Season(int n){
         this.number = n;
-        System.out.println("Parametrized Constructor Called");
+        System.out.println("Parametrized Constructor Initialized!!!");
     }
     void display(){
         System.out.println("Display method of Season Enum Class " + this.number);
